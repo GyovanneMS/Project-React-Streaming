bbae102bf6e19a06b040a76965451467dd263620
GyovanneMS <gy.martinssabara@gmail.com>
2023-10-21 12:34:15 -0300

    finalizando painel

--- proj07f/src/pages/Painel.jsx
+++ proj07f/src/pages/Painel.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Principal from "../components/Principal";
+import Formulario from "../components/Formulario";
 
 export default function Painel() {
     return <>
       <Principal fundo="bgcine.jpg" tamanho="488px">
         <Formulario/>
       </Principal>
-  
     </>
 }
